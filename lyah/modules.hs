8f53778f79211b85ba10f4d3f39089a5cbe05d01
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-01-28 06:27:33 +0300

    more list functions

--- lyah/modules.hs
+++ lyah/modules.hs
@@ -50,4 +50,48 @@ any (==4) [3,5,7,2,8]
 all (`elem` ['A'..'Z']) "HEYGUYSGreetings"
 --False
 
+--iterate: take a function and a start value and repeatedly apply that function each resulting value
+take 10 $ iterate (*2) 10
+--10,20,40,80,160,320,640,1280,2560,5120 
+
+--splitAt: take a number and a list. slit the list at the index of the number and give the resulting lists in a turple.
+splitAt 5 "tesaracting"
+--("tesar","acting")
+
+--takeWhile: takes elements from a list while a predicate holds. Stops when an element that does not satisfy the predicate is encountered.
+takeWhile (>2) [5,3,4,2,7,6,1]
+--[5,3,4]
+
+takeWhile (/='t') "This is not a test"
+--"This is no"
+
+--dropWhile: works in the opposite way to takeWhile. drops all the elements when the predicate is true. Returns the rest of the  list when the predicate turns false
+dropWhile (/=' ') "today is a good day"
+--" is a good day"
+
+--sort: sorts an element of typeclass Ord
+sort [3,5,2,8]
+--[2,3,5,8]
+
+--group: groups a list into adjacent sublists if they are equal
+group [1,1,2,2,3,4,4][1,1,2,2,3,4,4]
+[[1,1],[2,2],[3],[4,4],[1],[6]]
+
+--inits: returns all initial segments of the list argument recursively. Shortest first.
+inits "cowbell"
+["","c","co","cow","cowb","cowbe","cowbel","cowbell"]
+
+--tails: returns all the final segments of the list argument. longest first
+tails "cowbell"
+["cowbells","owbells","wbells","bells","ells","lls","ls","s",""]
+
+--using tails to create a function that searches a substring in a string
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack = 
+	let nlen = length needle
+	in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+--partition: takes a list and predicate and returns a pair of lists. First list has all elements that satisfy the predicate, second list has all that do not satisfy the predicate.
+partition (>3) [4,6,7,2,8,3,1,1,6,3]
+([4,6,7,8,6],[2,3,1,1,3])
 

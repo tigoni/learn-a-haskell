c99b7bf385960fbeba9cd99e1251ab0303ca2839
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-01-31 05:35:53 +0300

    begin on custom types

---
+++ lyah/custom_types.hs
@@ -0,0 +1,8 @@
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+
+--function takes a Shape and returns the surface
+surface :: Shape -> Float
+surface (Circle _ _ r) = pi * r ^ 2
+surface (Rectangle x1 y1 x2 y2)  = (abs $ x2 - x1) * (abs $ y2 - y1)
+

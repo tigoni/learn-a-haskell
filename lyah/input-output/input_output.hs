d8b64633395d743c36a6dc4071555fb78ab37a34
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-02-23 07:05:03 +0300

    refactor: new main function the reverses input line words

--- lyah/input-output/input_output.hs
+++ lyah/input-output/input_output.hs
@@ -9,17 +9,30 @@ import Data.Char (toUpper)
 --An IO action is performed when it is given the name of main and the program is run.
 -- do can be used to group several IO actions together
 
-main = do
-    putStrLn "Hello what's your first name?"
-    firstName <- getLine
-    putStrLn "What your second name?"
-    lastName <- getLine
-    let bigFirstName = map toUpper firstName
-        bigLastname = map toUpper lastName
-    putStrLn $ "Hey " ++ bigFirstName ++ " " ++ bigLastname ++ " how are you?"
+mainOld = do
+        putStrLn "Hello what's your first name?"
+        firstName <- getLine
+        putStrLn "What your second name?"
+        lastName <- getLine
+        let bigFirstName = map toUpper firstName
+            bigLastname = map toUpper lastName
+        putStrLn $ "Hey " ++ bigFirstName ++ " " ++ bigLastname ++ " how are you?"
 
 -- IO actions are only performed if they are given a name of main of they are inside a bigger IO action
 --block composed with an do block. A do block can be used to group together IO actions. 
 -- The resulting IO action can then be used in another do block. Finally all blocks have to fall into main to 
 -- be processed.
 
+
+
+--- program to continously read a line and print out the same line with the words reversed 
+-- the program stops when a blank line is input
+main = do
+    line <- getLine
+    if null line 
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words

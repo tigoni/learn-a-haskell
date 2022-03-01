4a3a6b6b9f1996c9ffccce7f875681081de5f330
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-03-01 05:15:25 +0300

    add: using the interact function

--- lyah/input-output/files_and_streams.hs
+++ lyah/input-output/files_and_streams.hs
@@ -3,14 +3,27 @@ import Control.Monad
 import Data.Char
 
 --program takes input and prints only shortLines
+-- main = do
+--     contents <- getContents
+--     putStr (shortLinesOnly contents)
+
+-- shortLinesOnly :: String -> String
+-- shortLinesOnly input = 
+--     let allLines = lines input
+--         shortLines = filter (\line -> length line < 10) allLines
+--         result = unlines shortLines
+--     in  result
+
+
+--shorter alternative
+-- main = do interact $ unlines . filter ((<10) .length) . lines
+
+--interact: takes a function of type String -> String -> IO ()
+
+--declare function
+countChars :: String -> String
+countChars xs = "total chars are " ++ show (length xs)
+
 main = do
-    contents <- getContents
-    putStr (shortLinesOnly contents)
-
-shortLinesOnly :: String -> String
-shortLinesOnly input = 
-    let allLines = lines input
-        shortLines = filter (\line -> length line < 10) allLines
-        result = unlines shortLines
-    in  result
-
+        putStrLn "Enter a string to get the count"
+        interact countChars

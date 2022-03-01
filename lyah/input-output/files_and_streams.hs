6116a4cf4e09fbe00ff6520f52c99fe4405c51b3
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-03-01 09:23:45 +0300

    add: function to test palindromes

--- lyah/input-output/files_and_streams.hs
+++ lyah/input-output/files_and_streams.hs
@@ -22,8 +22,15 @@ import Data.Char
 --applies the function on it and prints out the function's results.
 
 --usefull SO answer on how interact works: https://stackoverflow.com/questions/16799755/haskell-interact-function
-main = interact (unlines .map countChars.lines)
+-- main = interact (unlines .map countChars.lines)
 
 --declare function
 countChars :: String -> String
 countChars xs = show $ length xs
+
+
+--code that checks if an input line is a palindrome or not
+respondPalindromes = unlines .map (\xs -> if isPalindrome xs then "palindrome" else "not palindrome") . lines 
+    where isPalindrome xs = xs == reverse xs
+
+main = interact respondPalindromes
